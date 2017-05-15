62d793ec891dda3565c7a7b894c18d32fdc2a28f
paukl <killasdfkk@>
2017-05-15 09:27:50 +0200

    zzzz

---
+++ Boden.java
@@ -0,0 +1,20 @@
+import ea.*;
+
+public class Boden extends Figur
+{
+    public int xPos;
+    public int yPos;
+    public String URL;
+    
+    
+    public Boden(int x,int  y, String Link){
+        xPos = x;
+        yPos = y;
+        URL = Link;
+        
+        
+        
+    }
+    
+   
+}
--- Spiel.java
+++ Spiel.java
@@ -1,73 +1,80 @@
 import ea.*;
+import java.util.ArrayList;
 
 public class Spiel extends Game implements Ticker
 {
     private Bild Hintergrundbild1;
     private Bild Hintergrundbild2;
     private Bild Hintergrundbild3;
-    private Figur figur;
-    private Figur Boden1;
-    private Figur Boden2;
-    private Figur Boden3;
-    private Figur Boden4;
-    private Figur Boden5;
-    private Figur Boden6;
+    private Figur figurlaufen;
+    private float rechts;
+    private Figur Boden;
+    private Bild Kiste1;
+    private Bild Kiste2;
+    private Figur[] Bodenfeld;
     
    public Spiel(){
    super(1080, 720, "Fenstertitel");
+   rechts = 0;
   
    Hintergrundbild1 = new Bild(0, 0, "BackgroundSpace1.png");
-   Hintergrundbild2 = new Bild(800, 0, "BackgroundSpace1.png");
-   Hintergrundbild3 = new Bild(1600, 0, "BackgroundSpace1.png");
+   Hintergrundbild2 = new Bild(1080, 0, "BackgroundSpace1.png");
+   Hintergrundbild3 = new Bild(2160, 0, "BackgroundSpace1.png");
    
-   figur = new Figur(20, 20, "Figur1.eaf");
-   figur.aktivMachen();
-   figur.faktorSetzen(4);
+   figurlaufen = new Figur(250, 20, "laufen.eaf");
+   figurlaufen.aktivMachen();
+   figurlaufen.faktorSetzen(4);
    
-   Boden1 = new Figur(0, 500, "Stone90x5.eaf");
-   Boden1.passivMachen();
-   Boden1.faktorSetzen(3);
+   for(int i=0; i<10; i++){
+       Boden = new Boden(i*90, 500, "Stone90x5.eaf");
+    }
    
-   Boden2 = new Figur(90, 500, "Stone90x5.eaf");
-   Boden2.passivMachen();
-   Boden2.faktorSetzen(3);
    
-   Boden3 = new Figur(180, 500, "Stone90x5.eaf");
-   Boden3.passivMachen();
-   Boden3.faktorSetzen(3);
+   Kiste1 = new Bild(500, 470, 200, "KisteStyle20x20.png");
+   Kiste1.passivMachen();
    
-   Boden4 = new Figur(270, 500, "Stone90x5.eaf");
-   Boden4.passivMachen();
-   Boden4.faktorSetzen(3);
+   Kiste2 = new Bild(500, 440, 200, "KisteStyle20x20.png");
+   Kiste2.passivMachen();
    
-   Boden5 = new Figur(0, 500, "Stone90x5.eaf");
-   Boden5.passivMachen();
-   Boden5.faktorSetzen(3);
-   
-   Boden6 = new Figur(0, 500, "Stone90x5.eaf");
-   Boden6.passivMachen();
-   Boden6.faktorSetzen(3);
-   
-   wurzel.add(Hintergrundbild1, Hintergrundbild2, Hintergrundbild3, figur,
-   Boden1, Boden2, Boden3, Boden4, Boden5, Boden6);
-   manager.anmelden(this, 50);
+   wurzel.add(Hintergrundbild1, Hintergrundbild2, Hintergrundbild3, figurlaufen,
+   Boden, Kiste1, Kiste2);
+   manager.anmelden(this, 10);
    
 }
 
 public void tick(){
+  rechts = rechts + 0.5f;
   Hintergrundbild1.verschieben(-0.5f, 0);
   Hintergrundbild2.verschieben(-0.5f, 0);
   Hintergrundbild3.verschieben(-0.5f, 0);
+  Kiste1.verschieben(-0.5f, 0);
+  Kiste2.verschieben(-0.5f, 0);
+  if(figurlaufen.schneidet(Kiste1)){
+      zuruecksetzen();
+    }
+  if(figurlaufen.schneidet(Kiste2)){
+      zuruecksetzen();
+    }
 }
 
-public void tasteReagieren(int tastencode) {
-   
+public void zuruecksetzen(){
+  Hintergrundbild1.verschieben(rechts, 0);
+  Hintergrundbild2.verschieben(rechts, 0);
+  Hintergrundbild3.verschieben(rechts, 0);
+  Kiste1.verschieben(rechts, 0);
+  Kiste2.verschieben(rechts, 0);
+  rechts = 0;
+}
+
+
+
+public void tasteReagieren(int tastencode){
     switch(tastencode){
         
-        case Taste.OBEN: figur.sprung(4); break;
-        case Taste.UNTEN: figur.verschieben(+0, +10); break;
-        case Taste.RECHTS: figur.verschieben(+10, +0); break;
-        case Taste.LINKS: figur.verschieben(-10, +0); break;
+        case Taste.OBEN: figurlaufen.sprung(8); break;
+        case Taste.UNTEN: figurlaufen.verschieben(+0, +10); break;
+        case Taste.RECHTS: figurlaufen.verschieben(+10, +0); break;
+        case Taste.LINKS: figurlaufen.verschieben(-10, +0); break;
     }
 }
 }
