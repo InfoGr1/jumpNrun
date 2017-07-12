d90b9098bc492a1de01b37c542c1d6354413ef4f
paukl <killasdfkk@>
2017-07-12 13:19:58 +0200

    zzz

---
+++ Hindernisse.java
@@ -0,0 +1,14 @@
+import ea.*;
+
+public class Hindernisse extends Bild
+{
+    
+    public Hindernisse(int x, int y, String u){
+        super(x, y, u);
+        this.passivMachen();
+    }
+    
+    
+    
+    
+}
--- Spiel.java
+++ Spiel.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 public class Spiel extends Game implements Ticker
 {
     private Bild Hintergrundbild;
-
-    private Figur figurlaufen;
-    private float rechts;
+    public Figur figurlaufen;
     private Figur Boden;
+    private boolean halt;
+    private Bild[] Hinterfeld;
+    private Figur[] Bodenfeld;
+
     private Bild Kiste1;
     private Bild Kiste2;
     private Bild Kiste3;
     private Bild Kiste4;
+    private Bild Kiste5;
+    private Bild Kiste6;
+    private Bild Kiste7;
+    private Bild Kiste8;
     private Bild Treppe1;
     private Bild Treppe2;
     private Bild Treppe3;
@@ -22,14 +28,10 @@ public class Spiel extends Game implements Ticker
     private Bild Treppe8;
     private Bild Treppe9;
     private Bild Treppe10;
-    private Bild[] Hinterfeld;
-    private Figur[] Bodenfeld;
-    private boolean anhalten;
-
 
     public Spiel(){
         super(1080, 720, "Fenstertitel");
-        rechts = 0;
+        halt = false;
         Bodenfeld = new Figur[50];
         Hinterfeld = new Bild[50];
 
@@ -44,7 +46,6 @@ public class Spiel extends Game implements Ticker
             Hinterfeld[z+u] = Hintergrundbild;
             wurzel.add(Hintergrundbild);
         }
-
         for(int i=0; i<20; i++){
             Boden = new Figur(i*270, 720, "Stone270x5.eaf");
             Boden.faktorSetzen(3);
@@ -53,43 +54,29 @@ public class Spiel extends Game implements Ticker
             wurzel.add(Bodenfeld[i]);
         }
 
+        Kiste1 = new Hindernisse(660, 660, "KisteStyle60x60.png");
+        Kiste2 = new Hindernisse(720, 600, "KisteStyle60x60.png");
+        Kiste3 = new Hindernisse(780, 660, "KisteStyle60x60.png");
+        Kiste4 = new Hindernisse(720, 660, "KisteStyle60x60.png");
+        Treppe1 = new Hindernisse(1180, 680, "Treppenstufe50x40.png");
+        Treppe2 = new Hindernisse(1230, 680, "Treppenstufe50x40.png");
+        Treppe3 = new Hindernisse(1230, 640, "Treppenstufe50x40.png");
+        Treppe4 = new Hindernisse(1280, 680, "Treppenstufe50x40.png");
+        Treppe5 = new Hindernisse(1280, 640, "Treppenstufe50x40.png");
+        Treppe6 = new Hindernisse(1280, 600, "Treppenstufe50x40.png");
+        Treppe7 = new Hindernisse(1330, 680, "Treppenstufe50x40.png");
+        Treppe8 = new Hindernisse(1330, 640, "Treppenstufe50x40.png");
+        Treppe9 = new Hindernisse(1330, 600, "Treppenstufe50x40.png");
+        Treppe10 = new Hindernisse(1380, 680, "Treppenstufe50x40.png");
+
         figurlaufen = new Figur(540, 500, "laufen.eaf");
         figurlaufen.aktivMachen();
         figurlaufen.faktorSetzen(4);
         cam.fokusSetzen(figurlaufen);
 
-        Kiste1 = new Bild(660, 660, "KisteStyle60x60.png");
-        Kiste1.passivMachen();
-        Kiste2 = new Bild(720, 600, "KisteStyle60x60.png");
-        Kiste2.passivMachen();
-        Kiste3 = new Bild(780, 660, "KisteStyle60x60.png");
-        Kiste3.passivMachen();
-        Kiste4 = new Bild(720, 660, "KisteStyle60x60.png");
-        Kiste4.passivMachen();
-        Treppe1 = new Bild(1180, 680, "Treppenstufe50x40.png");
-        Treppe1.passivMachen();
-        Treppe2 = new Bild(1230, 680, "Treppenstufe50x40.png");
-        Treppe2.passivMachen();
-        Treppe3 = new Bild(1230, 640, "Treppenstufe50x40.png");
-        Treppe3.passivMachen();
-        Treppe4 = new Bild(1280, 680, "Treppenstufe50x40.png");
-        Treppe4.passivMachen();
-        Treppe5 = new Bild(1280, 640, "Treppenstufe50x40.png");
-        Treppe5.passivMachen();
-        Treppe6 = new Bild(1280, 600, "Treppenstufe50x40.png");
-        Treppe6.passivMachen();
-        Treppe7 = new Bild(1330, 680, "Treppenstufe50x40.png");
-        Treppe7.passivMachen();
-        Treppe8 = new Bild(1330, 640, "Treppenstufe50x40.png");
-        Treppe8.passivMachen();
-        Treppe9 = new Bild(1330, 600, "Treppenstufe50x40.png");
-        Treppe9.passivMachen();
-        Treppe10 = new Bild(1380, 680, "Treppenstufe50x40.png");
-        Treppe10.passivMachen();
-
-        wurzel.add(figurlaufen, Kiste1, Kiste2, Kiste3, Kiste4, 
-            Treppe1, Treppe2, Treppe3, Treppe4, Treppe5, Treppe6, 
-            Treppe7, Treppe8, Treppe9, Treppe10);
+        wurzel.add(figurlaufen, Kiste1, Kiste2, 
+        Kiste3, Kiste4, Treppe1, Treppe2, Treppe3, Treppe4, 
+        Treppe5, Treppe6, Treppe7, Treppe8, Treppe9, Treppe10);
         manager.anmelden(this, 10);
     }
 
@@ -98,16 +85,9 @@ public class Spiel extends Game implements Ticker
     }
 
     public void tick(){
-        if(figurlaufen.schneidet(Kiste1)){
-            anhalten = true;
-            
-        }else{
-            anhalten = false;
-        }
-        if(anhalten == false){
-            if(tasteGedrueckt(3)){
-                figurlaufen.verschieben(2, 0);
-            }
+        
+        if(tasteGedrueckt(3)){
+            figurlaufen.verschieben(2, 0);
         }
         if(tasteGedrueckt(22)){
             figurlaufen.sprung(8);
@@ -118,11 +98,10 @@ public class Spiel extends Game implements Ticker
         if(figurlaufen.getY()>2000){
             zuruecksetzen();
         }
-
         
-
     }
+
     public void tasteReagieren(int tastencode){
     }
-    
+
 }
