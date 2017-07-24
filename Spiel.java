a68f27b71b3130db671b7bb3efc75fa0a26f7c47
Luka <ki-lolo@freenet.de>
2017-07-24 09:26:42 +0200

    eqr

--- Spiel.java
+++ Spiel.java
@@ -46,7 +46,37 @@ public class Spiel extends Game implements Ticker
     private Bild Treppe19;
     private Bild Treppe20;
     private Bild Treppe21;
+    private Bild Kiste16;
+    private Bild Kiste17;
+    private Bild Kiste18;
+    private Bild Kiste19;
+    private Bild Kiste20;
+    private Bild Kiste21;
+    private Bild Kiste22;
+    private Bild Kiste23;
+    private Bild Kiste24;
+    private Bild Kiste25;
+    private Bild Kiste26;
+    private Bild Kiste27;
+    private Bild Kiste28;
+    private Bild Kiste29;
+    private Bild Kiste30;
+    private Bild Kiste31;
+    private Bild Kiste32;
+    private Bild Kiste33;
+    private Bild Kiste34;
+    private Bild Kiste35;
+    private Bild Kiste36;
+    private Bild Kiste37;
+    private Bild Kiste38;
+    private Bild Kiste39;
+    private Bild Kiste40;
+    private Bild Rakete;
 
+    public Figur flamme1;
+    public Figur flamme2;
+    public Figur Endboss;
+    public Figur figur2;
     private Figur Boden1;
     private Figur Boden2;
 
@@ -133,6 +163,32 @@ public class Spiel extends Game implements Ticker
         Treppe19 = new Hindernisse(2520, 640, "Treppenstufe50x40.png");
         Treppe20 = new Hindernisse(2520, 600, "Treppenstufe50x40.png");
         Treppe21 = new Hindernisse(2520, 560, "Treppenstufe50x40.png");
+        Kiste16 = new Hindernisse(260, 660, "transparent1.png");
+        Kiste17 = new Hindernisse(260, 600, "transparent1.png");
+        Kiste18 = new Hindernisse(260, 540, "transparent1.png");
+        Kiste19 = new Hindernisse(260, 480, "transparent1.png");
+        Kiste20 = new Hindernisse(260, 420, "transparent1.png");
+        Kiste21 = new Hindernisse(260, 360, "transparent1.png");
+        Kiste22 = new Hindernisse(260, 300, "transparent1.png");
+        Kiste23 = new Hindernisse(260, 240, "transparent1.png");
+        Kiste24 = new Hindernisse(260, 180, "transparent1.png");
+        Kiste25 = new Hindernisse(320, 180, "transparent1.png");
+        Kiste26 = new Hindernisse(380, 180, "transparent1.png");
+        Kiste27 = new Hindernisse(440, 180, "transparent1.png");
+        Kiste28 = new Hindernisse(500, 180, "transparent1.png");
+        Kiste29 = new Hindernisse(560, 180, "transparent1.png");
+        Kiste30 = new Hindernisse(620, 180, "transparent1.png");
+        Kiste31 = new Hindernisse(680, 180, "transparent1.png");
+        Kiste32 = new Hindernisse(740, 180, "transparent1.png");
+        Kiste33 = new Hindernisse(800, 180, "transparent1.png");
+        Kiste34 = new Hindernisse(880, 180, "transparent1.png");
+        Kiste35 = new Hindernisse(940, 180, "transparent1.png");
+        Kiste36 = new Hindernisse(1000, 180, "transparent1.png");
+        Kiste37 = new Hindernisse(1060, 180, "transparent1.png");
+        Kiste38 = new Hindernisse(1120, 180, "transparent1.png");
+        Kiste39 = new Hindernisse(1180, 180, "transparent1.png");
+        Kiste40 = new Hindernisse(1240, 180, "transparent1.png");
+
 
         Boden1 = new Figur(1940, 645, "Stone270x5.eaf");
         Boden1.faktorSetzen(3);
@@ -141,10 +197,40 @@ public class Spiel extends Game implements Ticker
         Boden2.faktorSetzen(3);
         Boden2.passivMachen();
 
-        figurlaufen = new Figur(540, 500, "laufen.eaf");
+        Rakete = new Bild(700, 250, "RaketeSkal.png");
+        Rakete.passivMachen();
+
+        figurlaufen = new Figur(4200, 500, "laufen.eaf");
         figurlaufen.aktivMachen();
         figurlaufen.faktorSetzen(4);
         cam.fokusSetzen(figurlaufen);
+
+       
+        //540, 500
+
+        figur2 = new Figur(4700, 500, "geistr.eaf");
+        figur2.aktivMachen();
+        figur2.faktorSetzen(3);
+
+        Endboss = new Figur(4300, 500, "EndBosz.eaf");
+        Endboss.aktivMachen();
+        Endboss.faktorSetzen(5);
+
+        
+        flamme1 = new Figur(4300, 620,"Flamme10x10.eaf");
+            flamme1.aktivMachen();
+            flamme1.faktorSetzen(2);
+            
+           flamme2= new Figur(4300, 700,"Flamme10x10.eaf");
+           flamme2.aktivMachen();
+            flamme2.faktorSetzen(2);
+            
+            
+           
+        
+
+           
+        
 
         wurzel.add(figurlaufen, Kiste1, Kiste2, 
             Kiste3, Kiste4, Kiste5, Kiste6, Kiste7, Kiste8, Kiste9, Kiste10, Kiste11, Kiste12,
@@ -152,15 +238,34 @@ public class Spiel extends Game implements Ticker
             Treppe1, Treppe2, Treppe3, Treppe4, Treppe5, Treppe6, Treppe7, 
             Treppe8, Treppe9, Treppe10, Treppe11, Treppe12, Treppe13, Treppe14, 
             Treppe15, Treppe16, Treppe17, Treppe18, Treppe19, Treppe20, Treppe21, 
-            Boden1, Boden2);
+            Boden1, Boden2, Rakete, Endboss, figur2, flamme1, flamme2, 
+            Kiste16, Kiste17, Kiste18, Kiste19, Kiste20, Kiste21, Kiste22, Kiste23, Kiste24, Kiste25,Kiste26, Kiste27, Kiste28, Kiste29, Kiste30, Kiste31, Kiste32, Kiste33, Kiste34, Kiste35, Kiste36, Kiste37, Kiste38, Kiste39, Kiste40);
         manager.anmelden(this, 10);
     }
 
     public void zuruecksetzen(){
         figurlaufen.positionSetzen(540, 500);
+        figur2.positionSetzen(4700, 500);
     }
 
     public void tick(){
+        figur2.verschieben(-0.5f, 0);
+         
+                flamme1.verschieben(-0.5f, 0);
+            
+                flamme2.verschieben(-0.5f, 0);
+            
+
+        if(figurlaufen.schneidet(figur2)){
+            zuruecksetzen();
+        }
+        
+        if(figurlaufen.schneidet(flamme1) || figurlaufen.schneidet(flamme2)){
+            zuruecksetzen();
+        }
+        
+        
+
         if(figurlaufen.schneidet(Kiste1)||figurlaufen.schneidet(Kiste2)||figurlaufen.schneidet(Kiste3)
         ||figurlaufen.schneidet(Kiste5)||figurlaufen.schneidet(Kiste6)||figurlaufen.schneidet(Kiste7)||
         figurlaufen.schneidet(Kiste8)||figurlaufen.schneidet(Kiste9)){
@@ -183,10 +288,9 @@ public class Spiel extends Game implements Ticker
             Kiste7.passivMachen();
             Kiste8.passivMachen();
             Kiste9.passivMachen();
+
             figurlaufen.sprung(8);
-
         }
-
         if(tasteGedrueckt(0)&&halt==false){
             figurlaufen.verschieben(-2, 0);
         }
@@ -217,5 +321,5 @@ public class Spiel extends Game implements Ticker
 
     public void tasteReagieren(int tastencode){
     }
-
 }
+
